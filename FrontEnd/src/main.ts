156eb49c783179046b514b7b0be77e6678586ca1
Gregorio Blando Ropellatto <gregorio.blando@mi.unc.edu.ar>
2023-01-15 21:10:34 -0300

    Firebase

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaWlLWcmr47hn8oTWvPcPhhj7LS2Oe8Oo",
+  authDomain: "proyectointegrador-frontend.firebaseapp.com",
+  projectId: "proyectointegrador-frontend",
+  storageBucket: "proyectointegrador-frontend.appspot.com",
+  messagingSenderId: "80905364687",
+  appId: "1:80905364687:web:6b780b5977074a195983ba",
+  measurementId: "G-F5CN9L9RSC"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
